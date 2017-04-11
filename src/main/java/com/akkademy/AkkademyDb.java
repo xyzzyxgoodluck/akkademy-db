b5571be752842ae06c9fb375ebe4888c8756e146
xyzzyxgoodluck <yongxiangtiger@163.com>
2017-04-11 14:20:25 +0800

    initial version

---
+++ src/main/java/com/akkademy/AkkademyDb.java
@@ -0,0 +1,25 @@
+package com.akkademy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.akkademy.messages.SetRequest;
+
+import akka.actor.AbstractActor;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+import akka.japi.pf.ReceiveBuilder;
+
+public class AkkademyDb extends AbstractActor {
+	
+	protected final LoggingAdapter log = Logging.getLogger(context().system(), this);
+	
+	protected final Map<String, Object> map = new HashMap<>();
+
+	private AkkademyDb() {
+		receive(ReceiveBuilder.match(SetRequest.class, message -> {
+			log.info("Received set request – key: {} value: {}", message.getKey(), message.getValue());
+			map.put(message.getKey(), message.getValue());
+		}).matchAny(o -> log.info("received unknownmessage {}", o)).build());
+	}
+}
