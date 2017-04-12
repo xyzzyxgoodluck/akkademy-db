17788869f4fc80b5ed3f894a40148f82bc07b531
xyzzyxgoodluck <yongxiangtiger@163.com>
2017-04-12 13:36:50 +0800

    add new feature

--- src/main/java/com/akkademy/AkkademyDb.java
+++ src/main/java/com/akkademy/AkkademyDb.java
@@ -3,23 +3,35 @@ package com.akkademy;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.akkademy.messages.GetRequest;
+import com.akkademy.messages.KeyNotFoundException;
 import com.akkademy.messages.SetRequest;
 
 import akka.actor.AbstractActor;
+import akka.actor.Status;
 import akka.event.Logging;
 import akka.event.LoggingAdapter;
 import akka.japi.pf.ReceiveBuilder;
 
 public class AkkademyDb extends AbstractActor {
-	
+
 	protected final LoggingAdapter log = Logging.getLogger(context().system(), this);
-	
+
 	protected final Map<String, Object> map = new HashMap<>();
 
 	private AkkademyDb() {
-		receive(ReceiveBuilder.match(SetRequest.class, message -> {
-			log.info("Received set request – key: {} value: {}", message.getKey(), message.getValue());
-			map.put(message.getKey(), message.getValue());
-		}).matchAny(o -> log.info("received unknownmessage {}", o)).build());
+		receive(ReceiveBuilder
+				.match(SetRequest.class, message -> {
+					log.info("Received Set request: {}", message);
+					map.put(message.getKey(), message.getValue());
+					sender().tell(new Status.Success(message.getKey()), self());
+				})
+				.match(GetRequest.class, message -> {
+					log.info("Received Get request: {}", message);
+					String value = (String)map.get(message.key);
+					Object response = (value != null) ? value : new Status.Failure(new KeyNotFoundException(message.key));
+					sender().tell(response, self());
+				})
+				.matchAny(o -> sender().tell(new Status.Failure(new ClassNotFoundException()), self())).build());
 	}
 }
--- src/main/java/com/akkademy/messages/SetRequest.java
+++ src/main/java/com/akkademy/messages/SetRequest.java
@@ -1,12 +1,16 @@
 package com.akkademy.messages;
 
-public class SetRequest {
+public class SetRequest implements java.io.Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2211224161876737948L;
 	private final String key;
 	private final Object value;
 
 	public SetRequest(String key, Object value) {
-	this.key = key;
-	this.value = value;
+		this.key = key;
+		this.value = value;
 	}
 
 	public String getKey() {
