f2b9ff7638c39eba62754c2616ea5cde75893593
xyzzyxgoodluck <yongxiangtiger@163.com>
2017-04-12 13:33:58 +0800

    application entry point

---
+++ src/main/java/com/akkademy/Main.java
@@ -0,0 +1,13 @@
+package com.akkademy;
+
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+
+public class Main {
+
+	public static void main(String... args) {
+		ActorSystem system = ActorSystem.create("akkademy");
+		system.actorOf(Props.create(AkkademyDb.class), "akkademy-db");
+	}
+
+}
